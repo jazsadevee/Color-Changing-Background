73d3db1a787f5e1d66b929b4cdd081919769871e
JazSade Villian <jazsadevee@gmail.com>
2023-02-09 13:05:52 -0500

    Make button center of the page

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-let index = 0;
+// let index = 0;
 
-function changeColors() {
-  let colors = ["red", "blue", "yellow", "green", "purple", "orange"]
+// function changeColors() {
+//   let colors = ["red", "blue", "yellow", "green", "purple", "orange"]
 
+//   document.getElementsByTagName("body")[0].style.background = colors[index++];
+
+//   if (index > colors.length - 1)
+//     index = 0;
+// }
+
+
+let index = 0
+
+function changeColor() {
+  let colors = ["red", "blue", "white", "black", "yellow", "purple", "green", "pink"]
+  console.log(changeColor);
+  
   document.getElementsByTagName("body")[0].style.background = colors[index++];
-
+  
   if (index > colors.length - 1)
     index = 0;
 }
